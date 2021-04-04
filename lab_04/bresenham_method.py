336c319e68c26ad63bacc1fc317afabd62e0a678
amunra23 <ivancvet2001@gmail.com>
2021-04-04 23:17:35 +0300

    Fixed collisions in Bresenham

--- lab_04/bresenham_method.py
+++ lab_04/bresenham_method.py
@@ -33,27 +33,25 @@ def bresenham_circle(dot_c, radius, color):
 
     delta_i = 2 * (1 - radius)
 
-    edge = round(radius / sqrt(2))
-
     dots = []
 
     eps = 0
 
-    while (y > edge):
+    while (x < y + 1):
 
         add_dots_bres_circle(dots, [x_c, y_c], [x, y], color)
 
         if (delta_i < 0):
             eps = 2 * delta_i + 2 * y - 1
 
-            if (eps <= 0):
+            if (eps < 0):
                 param = 1
             else:
                 param = 2
         elif (delta_i > 0):
             eps = 2 * delta_i - 2 * x - 1
 
-            if (eps <= 0):
+            if (eps < 0):
                 param = 2
             else:
                 param = 3
@@ -95,10 +93,11 @@ def bresenham_ellipse(dot_c, rad, color):
     x = 0
     y = rad[1]
 
-    r_a = rad[0] * rad[0]
-    r_b = rad[1] * rad[1]
+    r_a_2 = rad[0] * rad[0]
+    r_b_2 = rad[1] * rad[1]
 
-    delta_i = r_a + r_b - r_a * (2 * y)
+    #delta_i = r_a + r_b - r_a * (2 * y)
+    delta_i = r_b_2 - r_a_2 * (2 * y + 1)
 
     dots = []
 
@@ -109,16 +108,16 @@ def bresenham_ellipse(dot_c, rad, color):
         add_dots_bres_ellipse(dots, [x_c, y_c], [x, y], color)
 
         if (delta_i < 0):
-            eps = 2 * delta_i + (2 * y - 1) * r_a
+            eps = 2 * delta_i + (2 * y + 2) * r_a_2
 
-            if (eps <= 0):
+            if (eps < 0):
                 param = 1
             else:
                 param = 2
         elif (delta_i > 0):
-            eps = 2 * delta_i + (- 2 * x - 1) * r_b
+            eps = 2 * delta_i + (- 2 * x + 2) * r_b_2
 
-            if (eps <= 0):
+            if (eps < 0):
                 param = 2
             else:
                 param = 3
@@ -127,14 +126,71 @@ def bresenham_ellipse(dot_c, rad, color):
 
         if (param == 1):
             x = x + 1
-            delta_i = delta_i + (2 * x) * r_b + r_b
+            delta_i = delta_i + (2 * x) * r_b_2 + r_b_2
         elif (param == 2):
             x = x + 1
             y = y - 1
-            delta_i = delta_i + (2 * x) * r_b - (2 * y + 2) * r_a + (r_a + r_b)
+            delta_i = delta_i + (2 * x) * r_b_2 - (2 * y) * r_a_2 + (r_a_2 + r_b_2)
         else:
             y = y - 1
-            delta_i = delta_i - (2 * y + 2) * r_a + r_a
+            delta_i = delta_i - (2 * y) * r_a_2 + r_a_2
 
 
     return dots
+
+
+# def reflect_dots_diag(dots, xc, yc):
+#     count_dots = len(dots)
+
+#     for i in range(count_dots):
+#         dots.append([dots[i][1] - yc + xc, dots[i][0] - xc + yc, dots[i][2]])
+
+# def reflect_dots_Oy(dots, xc):
+#     count_dots = len(dots)
+
+#     for i in range(count_dots):
+#         dots.append([-(dots[i][0] - xc) + xc, dots[i][1], dots[i][2]])
+
+# def reflect_dots_Ox(dots, yc):
+#     count_dots = len(dots)
+
+#     for i in range(count_dots):
+#         dots.append([dots[i][0], -(dots[i][1] - yc) + yc, dots[i][2]])
+    
+
+# def bresenham_ellipse(dot_c, rad, color):
+#     x = 0
+#     y = rad[1]
+#     dots = [[x + dot_c[0], y + dot_c[1], color]]
+
+#     xc = dot_c[0]
+#     yc = dot_c[1]
+
+#     sqr_ra = rad[0] * rad[0]
+#     sqr_rb = rad[1] * rad[1]
+#     delta = sqr_rb - sqr_ra * (2 * rad[1] + 1)
+
+#     while y >= 0:
+#         if delta <= 0:
+#             d = 2 * delta + sqr_ra * (y + y + 2)
+#             x += 1
+#             delta += sqr_rb * (2 * x + 1)
+
+#             if d >= 0:
+#                 y -= 1
+#                 delta += sqr_ra * (- y - y + 1)
+#         else:
+#             d = 2 * delta + sqr_rb * (- x - x + 2)
+#             y -= 1
+#             delta += sqr_ra * (- y - y + 1)
+
+#             if d < 0:
+#                 x += 1
+#                 delta += sqr_rb * (2 * x + 1)
+
+#         dots.append([x + xc, y + yc, color])
+
+#     reflect_dots_Oy(dots, xc)
+#     reflect_dots_Ox(dots, yc)
+
+#     return dots
